0f30370177c464be1d406fcd3f684459be48091a
Claire Vita <clairej.vita@gmail.com>
2021-03-11 22:26:37 -0500

    full video functionality

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,32 +1,36 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './video.css'
 const Video = () => {
-  const videoRef = useRef(null);
+    const videoRef = useRef(null);
+    useEffect(() => {
+        getVideo();
+    }, [videoRef]);
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
-
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 300 } })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error("error:", err);
-      });
-  };
-
-  return (
-    <div>
-      <div id="container">
-        <video ref={videoRef} />
-      </div>
-    </div>
-  );
+    const getVideo = () => {
+        navigator.mediaDevices
+            .getUserMedia({ video: { width: 300 } })
+            .then(stream => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(err => {
+                console.error("error:", err);
+            });
+    };
+    function handleClick(e) {
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <div id="container">
+                <video ref={videoRef} />
+                <button onClick={handleClick}>
+                    Toggle Camera
+      </button>
+            </div>
+        </div>
+    );
 };
 
 export default Video;
